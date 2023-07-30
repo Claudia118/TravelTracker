cc96d8acfa249792d029d4c8a075c2089582b1f1
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-07-29 23:00:09 -0600

    Edit Pages and added images

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
-import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
-      <PageNav />
-      <h1>TravelTracker</h1>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          TravelTracker
+          <br /> keeps track of your adventures.
+        </h1>
+        <h2>
+          Never forget your wonderful experiences, and show your friends how you
+          have wandered the world.
+        </h2>
 
-      <Link to="/app">Go to the app</Link>
-    </div>
+        <Link to="./app" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
