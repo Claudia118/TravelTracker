99a06cc781375e0fae0c307c39877732a0963a2d
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-07-31 11:15:18 -0600

    Added  nested and index routes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import styles from "./Footer.modules.css";
+import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <div className={styles.footer}>
+    <footer className={styles.footer}>
       <p className={styles.copyright}>
         &copy; Copyright {new Date().getFullYear()} by TravelTracker Inc
       </p>
-    </div>
+    </footer>
   );
 }
 
