58083bf38257271e8d2603ee631e4e43e1c193af
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-07-31 10:29:15 -0600

    Added components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+    </div>
+  );
+}
+
+export default Sidebar;
