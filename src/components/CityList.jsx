c3855293d730fbbd2f24460fa500251d3404ed2d
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-08-04 13:41:56 -0600

    using the CitiesContext

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,19 +2,24 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
-  if(isLoading) return <Spinner/>;
+function CityList() {
+  const { cities, isLoading } = useCities();
+  if (isLoading) return <Spinner />;
 
-  if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
 
-  return(
+  return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
-      <CityItem city={city} key={city.id} />
+        <CityItem city={city} key={city.id} />
       ))}
-  </ul>
- );
+    </ul>
+  );
 }
 
 export default CityList;
